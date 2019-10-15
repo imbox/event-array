ce09d99bb89353913c7f10921c26c5cbc8a17c37
Mathias Lundell <lundell.mathias@gmail.com>
2019-10-15 17:00:42 +0200

    style: add line between class methods

--- index.js
+++ index.js
@@ -18,42 +18,55 @@ class EventArray extends Array {
   emit (eventName, ...args) {
     return this.emitter.emit(eventName, ...args)
   }
+
   eventNames () {
     return this.emitter.eventNames()
   }
+
   getMaxListeners () {
     return this.emitter.getMaxListeners()
   }
+
   listenerCount (eventName) {
     return this.emitter.listenerCount(eventName)
   }
+
   listeners (eventName) {
     return this.emitter.listeners(eventName)
   }
+
   off (eventName, listener) {
     return this.emitter.off(eventName, listener)
   }
+
   on (eventName, listener) {
     return this.emitter.on(eventName, listener)
   }
+
   once (eventName, listener) {
     return this.emitter.once(eventName, listener)
   }
+
   prependListener (eventName, listener) {
     return this.emitter.prependListener(eventName, listener)
   }
+
   prependOnceListener (eventName, listener) {
     return this.emitter.prependOnceListener(eventName, listener)
   }
+
   removeAllListeners (...eventNames) {
     return this.emitter.removeAllListeners(...eventNames)
   }
+
   removeListener (eventName, listener) {
     return this.emitter.removeListener(eventName, listener)
   }
+
   setMaxListeners (n) {
     return this.emitter.setMaxListeners(n)
   }
+
   rawListeners (eventName) {
     return this.emitter.rawListeners(eventName)
   }
